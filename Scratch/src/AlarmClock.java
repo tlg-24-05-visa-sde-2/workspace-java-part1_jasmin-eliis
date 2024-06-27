016f6a7c7ec3145f4a20e6bccd40f4208fce8dbb
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-06-27 13:25:15 -0400

    initial alrmclock commit

---
+++ Scratch/src/AlarmClock.java
@@ -0,0 +1,9 @@
+class AlarmClock {
+    //properties or attributes these are called "instance variables" or "fields" in Java
+    int snoozeInterval = 5;
+
+    //functions or operations
+    void snooze() {
+        System.out.println("Snoozing for " + snoozeInterval + " minutes");
+    }
+}
