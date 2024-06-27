4638a6d58c27392c5e115944a11078e56793c57b
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-06-27 11:47:15 -0400

    'initial commit'

---
+++ Lab01.1/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ *Classic Hello World application in Java.
+ * This is a standalone application, i.e it does not run on an app server.
+ *
+ */
+class HelloWorld {
+ //starting point for any standalone Java application
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); //prints to console
+        System.out.println("This is nicer than JavaScript");
+
+    }
+
+}
