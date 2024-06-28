b6ba51742823387ae4723dbd7b8739ad4968a844
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-06-28 17:53:11 -0400

    getters and setters practice, private and public methods,

--- Scratch/src/AlarmClock.java
+++ Scratch/src/AlarmClock.java
@@ -1,9 +1,25 @@
 class AlarmClock {
     //properties or attributes these are called "instance variables" or "fields" in Java
-    int snoozeInterval = 5;
+    private int snoozeInterval = 5;
+
+    public int getSnoozeInterval() {
+        return snoozeInterval;
+    }
+
+    public void setSnoozeInterval(int snoozeInterval) {
+        this.snoozeInterval = snoozeInterval;
+    }
 
     //functions or operations
-    void snooze() {
+    public void snooze() {
         System.out.println("Snoozing for " + snoozeInterval + " minutes");
+    }
+
+    /*
+     * Purpose: return a string (sentence) describing the object
+     */
+
+    public String toString() {
+        return "AlarmClock: snoozeInterval = " + snoozeInterval;
     }
 }
--- Scratch/src/AlarmClockClient.java
+++ Scratch/src/AlarmClockClient.java
@@ -2,10 +2,10 @@ class AlarmClockClient {
 
     public static void main(String[] args) {
         AlarmClock clock1 = new AlarmClock();
-        clock1.snoozeInterval = 7;
+        clock1.setSnoozeInterval(7);
 
         AlarmClock clock2 = new AlarmClock();
-        clock2.snoozeInterval = 10;
+        clock2.setSnoozeInterval(10);
 
         AlarmClock clock3 = new AlarmClock();
 
@@ -14,5 +14,10 @@ class AlarmClockClient {
         clock1.snooze();
         clock2.snooze();
         clock3.snooze();
+        System.out.println();
+
+        System.out.println(clock1.toString());
+        System.out.println(clock2);
+        System.out.println(clock3);
     }
 }
