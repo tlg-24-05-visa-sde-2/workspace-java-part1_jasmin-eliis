823aaf99bf93a427c69a456aae88f8bbb198ec90
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-06-27 21:03:01 -0400

    alarm clock update

--- Scratch/src/AlarmClockClient.java
+++ Scratch/src/AlarmClockClient.java
@@ -1,20 +1,18 @@
 class AlarmClockClient {
 
-    public static void main(String[] args)
-        AlarmClock clock1 = new AlarmClcok();
+    public static void main(String[] args) {
+        AlarmClock clock1 = new AlarmClock();
         clock1.snoozeInterval = 7;
 
         AlarmClock clock2 = new AlarmClock();
         clock2.snoozeInterval = 10;
 
         AlarmClock clock3 = new AlarmClock();
-        clock3.snoozeInterval;
+
 
 
         clock1.snooze();
         clock2.snooze();
         clock3.snooze();
-
-
-
+    }
 }
