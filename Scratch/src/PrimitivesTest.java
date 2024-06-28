0b1669cfb80c9b10fd4cb425e460113234baae8e
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-06-27 22:11:24 -0400

    learn/experiment about primitive types, some work with strings and char

---
+++ Scratch/src/PrimitivesTest.java
@@ -0,0 +1,52 @@
+import java.sql.SQLOutput;
+
+class PrimitivesTest {
+
+    public static void main(String[] args) {
+        int age =34;
+        System.out.println("Age is: " + age);
+
+        long population = 8_000_000_000L;
+        System.out.println("Population is: " + population);
+
+        double price = 799.99;
+        System.out.println("Price is: " + price);
+
+        boolean isCloudy = false;
+        System.out.println("isCloudy is: " + isCloudy);
+
+        char size = 'S';
+        System.out.println("Size is: " + size);
+
+        String grade = "A";
+        System.out.println("My grade on the test was: " + grade);
+
+        String quote ="What's up, \"Doc\"?";
+        System.out.println("Bugs Bunny says: " + quote);
+
+        String poem = "roses are red\nviolets are blue";
+        System.out.println("roses are red\nviolets are blue");
+        System.out.println();
+
+
+        int n = 18;
+        int m = n;
+
+        n = 19;
+        System.out.println("n is: " + n);
+        System.out.println("m is: " + m);
+
+        int kids = 2;
+        System.out.println("Number of kids is: " + ++kids);
+
+    }
+}
+
+
+
+
+
+
+
+
+
