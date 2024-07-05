e2153d910a9243b5f75b74a521e8c2df8e6700f0
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-07-05 08:57:54 -0500

    Bug fixes for whiteboard, TODOs: Testing, constructors,enums

--- WhiteboardModel/src/Smartphone.java
+++ WhiteboardModel/src/Smartphone.java
@@ -1,16 +1,71 @@
 class Smartphone {
     //properties or attributes ("instance variables" or "fields")
-    String brand;
-    String model;
-    double price;
-    int releaseYear;
-    boolean isDamaged;
-    int memory;
-
+    private String model;
+    private double price;
+    private int releaseYear;
+    private String color = "Black";     //default color is black
 
 
     //functions or operations ("methods")
-    void sendText() {
-        System.out.println("Text message sent successfully from your " + model + ".");
+    public void sendText() {
+        System.out.println("Text message has been delivered from your " + model + ".");
     }
+
+    public void answerCall() {
+        System.out.println("Call answered by your " + model + ".");
+    }
+
+    public void takePhoto() {
+        System.out.println("Photo successfully taken from your " + model + ".");
+    }
+
+    //constructors
+    public Smartphone(String model, double price, int releaseYear, String color) {
+        this.model = model;
+        this.price = price;
+        this.releaseYear = releaseYear;
+        this.color = color;
+    }
+
+
+//accessor methods - controlled access to the objects fields
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return "Smartphone: " + model + " Release Year: " + releaseYear + " Price: " + price + "\n";
+    }
+
 }
--- WhiteboardModel/src/SmartphoneClient.java
+++ WhiteboardModel/src/SmartphoneClient.java
@@ -5,32 +5,54 @@
 class SmartphoneClient {
     //entry point for the application
     public static void main(String[] args) {
+
+
         //create an instance of smartphone
-        Smartphone phone1 = new Smartphone();
-        phone1.brand = "Apple";
-        phone1.model = "iPhone 12";
-        phone1.price = 649.99;
-        phone1.releaseYear = 2020;
-        phone1.memory = 128; // in GB
-        phone1.isDamaged = false;
+        Smartphone phone1 = new Smartphone("Apple iPhone 12", 649.99, 2020, "Black");
 
-        Smartphone phone2 = new Smartphone();
-        phone2.brand = "Samsung";
-        phone2.model = "Galaxy Z Fold5";
-        phone2.price = 1919.99;
-        phone2.releaseYear = 2023;
-        phone2.memory = 512;
-        phone2.isDamaged = false;
+//        phone1.price = 649.99;
+//        phone1.releaseYear = 2020;
+//        phone1.color = "Black";
 
-        Smartphone phone3 = new Smartphone();
-        phone3.brand = "Google";
-        phone3.model = "Pixel 8 Pro";
-        phone3.price = 1399.00;
-        phone3.releaseYear = 2023;
-        phone3.memory = 1000;
-        phone3.isDamaged = false;
+        //create a 2nd instance of smartphone
+        Smartphone phone2 = new Smartphone("Samsung Galaxy Z Fold5", 1919.99, 2023, "White");
+//        phone2.model = "Samsung Galaxy Z Fold5";
+//        phone2.price = 1919.99;
+//        phone2.releaseYear = 2023;
+//        phone2.color = "White";
+
+        //create a 3rd instance of smartphone
+        Smartphone phone3 = new Smartphone("Google Pixel 8 Pro", 1399.00, 2023, "Blue");
+//        phone3.model = "Google Pixel 8 Pro";
+//        phone3.price = 1399.00;
+//        phone3.releaseYear = 2023;
+//        phone3.color = "Blue";
+
+
+
 
         phone1.sendText();
+        phone1.answerCall();
+        phone1.takePhoto();
+        System.out.println(phone1);
+        System.out.println(phone1);
+        System.out.println();
+
+
+
+        phone2.sendText();
+        phone2.answerCall();
+        phone2.takePhoto();
+        System.out.println(phone2);
+        System.out.println();
+
+        phone3.sendText();
+        phone3.answerCall();
+        phone3.takePhoto();
+        System.out.println(phone3);
+        System.out.println();
+
+
     }
 
 }
