e4fab65dc94c321aa27ae3fb106ce438b1c6c3f0
Jasmin Ellis <JASMINE@tlgcohort.com>
2024-07-14 22:18:40 -0400

    Additional practice with try-catch-finally  and punting vs handling

---
+++ Part1_Lab10-Extra_Exceptions/Lab10-Extra/src/com/transportation/BoatClient.java
@@ -0,0 +1,10 @@
+package com.transportation;
+
+class BoatClient {
+    public static void main(String[] args) {
+        Boat boat = new Boat("Red Dragon");
+        boat.moveTo("West Seattle");
+
+    }
+
+}
